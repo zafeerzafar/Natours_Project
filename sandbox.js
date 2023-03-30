512e270928a78a750975c14cbb6acd4a503503ed
zafeerzafar <76705694+zafeerzafar@users.noreply.github.com>
2023-03-31 02:13:19 +0500

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,12 @@
+// console.log("Hello World");
+
+// let a = 70;
+// console.log(a - 1);
+
+// const b = document.querySelector('.navigation');
+
+// b.classList.add("u-margin-bottom-medium");
+
+
+
+
